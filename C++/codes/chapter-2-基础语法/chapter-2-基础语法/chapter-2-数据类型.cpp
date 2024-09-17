c682802d7c5cd330aeaff7fa70441bea5b0850b4
Lee <lish929@163.com>
2024-09-17 22:30:27 +0800

    更新C++代码与文档

---
+++ C++/codes/chapter-2-基础语法/chapter-2-基础语法/chapter-2-数据类型.cpp
@@ -0,0 +1,24 @@
+//#include <iostream>
+//
+//int main() {
+//	//// 定义变量
+//	//int sum;
+//	//// 定义变量并赋值
+//	//int num1 = 0;
+//	//int num2 = 0;
+//	//// 赋值
+//	//sum = num1 + num2;
+//	//std::cout << "sum:" << sum << std::endl;
+//
+//	char a[10] = "a";
+//	short int b = 10;
+//	int c = 10;
+//	long int d = 10;
+//	float e = 10.0;
+//	double f = 10.0;
+//	long double g = 10.0;
+//	bool h = false;
+//	wchar_t i[10] = L"a";
+//
+//	return 0;
+//}
